a31e45b5e721c7c54e58c50e8e046320a6449cba
Jonghoon Seo <jonghoon.seo@wisefour.com>
2021-09-29 13:43:47 +0900

    (DT) Add feature manager
    Change-Id: Ia532b5608037c588df14819078abe8553246077e

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 This directory contains deep learning based stock trading system.
 
 Design doc: https://docs.google.com/document/d/1TDrU1CxfyJ7QBZY_WBpRHaeVn7L3LtNp1viIoAfse70/edit#heading=h.e3nb2v558mw2
+
+## Prerequisites
+
+To execute this repo, we temporalily use virtualenv.
+
+```bash
+$ pyenv virtualenv 3.7.9 deeptraders
+$ pyenv activate deeptraders
+$ pip install -r requirements.txt
+```
--- feature_manager.py
+++ feature_manager.py
@@ -1,12 +1,44 @@
 """Manage various features for stock trading"""
-
-from absl import logging
+import abc
+from typing import Optional
+import FinanceDataReader as fdr
+import pandas as pd
 
 
 class FeatureManager:
+    CANDLE_FEATURES = ['Open', 'Close', 'High', 'Low', 'Volume', 'Change']
+    COMPANY_FEATURES = []
+    FEATURES = CANDLE_FEATURES + COMPANY_FEATURES
 
-    def __init__(self):
-        pass
+    def __init__(self, market: str):
+        """Initialize feature manager"""
+        self._market = market
 
-    def get_feature(self, code: str, feature_name: str, when: int):
-        logging.info(f'Get {feature_name} for {code} at {when}')
+    @abc.abstractmethod
+    def get_feature(self,
+                    code: str,
+                    feature_name: str,
+                    start: Optional[str] = None,
+                    end: Optional[str] = None):
+        raise NotImplementedError()
+
+
+# TODO(jseo): Separate as another modulea
+class FinanceDataReaderManager(FeatureManager):
+
+    def get_feature(self,
+                    code: str,
+                    feature_name: str,
+                    start: Optional[str] = None,
+                    end: Optional[str] = None):
+        """Get feature value"""
+        if feature_name not in self.CANDLE_FEATURES:
+            raise ValueError('Invalid feature request: {feature_name}')
+        return self.get_candle_data(code, start, end)[feature_name]
+
+    def get_candle_data(self,
+                        code: str,
+                        start: Optional[str] = None,
+                        end: Optional[str] = None) -> pd.DataFrame:
+        """Get Candle data"""
+        return fdr.DataReader(code, start, end)
